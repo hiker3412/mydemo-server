f6ebe8e8d9ceefddda61d5bd237fed1676f3d28c
hiker <hiker3412@gmail.com>
2024-05-25 07:41:14 +0800

    Flux test

--- mydemo-server-api/src/test/java/io/hiker/server/api/projectreactor/FluxTest.java
+++ mydemo-server-api/src/test/java/io/hiker/server/api/projectreactor/FluxTest.java
@@ -2,8 +2,8 @@ package io.hiker.server.api.projectreactor;
 
 
 import org.junit.jupiter.api.Test;
-import org.springframework.context.annotation.Configuration;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
 import java.time.Duration;
